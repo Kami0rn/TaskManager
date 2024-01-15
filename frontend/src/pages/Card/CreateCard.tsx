0cf90df0b09cb2f7185074489305c1bf498c3478
Kami0rn <tew965@gmail.com>
2024-01-16 02:30:14 +0700

    Create Card

---
+++ frontend/src/pages/Card/CreateCard.tsx
@@ -0,0 +1,130 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+import "sweetalert2/dist/sweetalert2.min.css"; // Import SweetAlert2 CSS
+import "./CreateList.css";
+
+function CreateCard() {
+  const navigate = useNavigate();
+  const [listName, setListName] = useState("");
+  const [listDescription, setListDescription] = useState("");
+
+  const handleCreateList = async () => {
+    try {
+      const projectId = localStorage.getItem("projectId");
+      const token = localStorage.getItem("token");
+      console.log("projectId:", projectId);
+
+      if (!projectId) {
+        console.error("projectId not found in localStorage");
+        return;
+      }
+
+      if (!token) {
+        navigate("/login");
+        return;
+      }
+
+      console.log("Data to be sent:", {
+        projectId,
+        listName,
+        listDescription,
+      });
+
+      const response = await fetch(
+        `http://localhost:8080/users/createList/${projectId}`,
+        {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            listName,
+            listDescription,
+          }),
+        }
+      );
+
+      if (!response.ok) {
+        const errorResponse = await response.json().catch(() => ({}));
+
+        if (errorResponse && errorResponse.error) {
+          // Display the error message using SweetAlert
+          Swal.fire({
+            html: `<i>${errorResponse.error}</i>`,
+            icon: "error",
+            customClass: {
+              container: "swal-container",
+            },
+          });
+          return;
+        }
+
+        throw new Error(
+          `HTTP error! Status: ${response.status}, Error: ${JSON.stringify(
+            errorResponse
+          )}`
+        );
+      }
+
+      const responseData = await response.json();
+      console.log("List created successfully:", responseData);
+
+      Swal.fire({
+        html: `<i>${responseData.message}</i>`,
+        icon: "success",
+        customClass: {
+          container: "swal-container",
+        },
+      }).then((result) => {
+        if (result.isConfirmed) {
+          // Refresh the page if the status is OK
+          if (response.status === 200) {
+            window.location.reload();
+          }
+        }
+      });
+
+      // Add any additional logic, such as closing the form or updating state
+    } catch (error) {
+      console.error("Error creating list:", error);
+    }
+  };
+
+  return (
+    <div>
+      <h2 className="text-cyan-500">Create List</h2>
+      <form>
+        <label>
+          List Name:
+          <input
+            className="my-2 bg-slate-200"
+            type="text"
+            value={listName}
+            onChange={(e) => setListName(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          List Description:
+          <textarea
+            className="my-2 bg-slate-200"
+            value={listDescription}
+            onChange={(e) => setListDescription(e.target.value)}
+          />
+        </label>
+        <br />
+        <button
+          type="button"
+          className="my-4 w-40 rounded-md bg-slate-400"
+          onClick={handleCreateList}
+        >
+          Create List
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default CreateCard;
